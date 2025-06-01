70d0bfeed3e023d947df23ec2b277fb9c8a977e1
fengzhongchen <fengzhongchen.ypa@gmail.com>
2025-06-01 12:46:52 +0800

    创建博客模板

---
+++ site.config.ts
@@ -0,0 +1,72 @@
+import { defineSiteConfig } from 'valaxy'
+
+export default defineSiteConfig({
+  url: 'https://fengzhongchen.github.io',
+  lang: 'zh-CN',
+  title: '风中尘的博客',
+  author: {
+    name: '风中尘',
+  },
+  description: '记录我遇到的问题和解决方案，以及一些有趣的事。',
+  social: [
+    {
+      name: 'RSS',
+      link: '/atom.xml',
+      icon: 'i-ri-rss-line',
+      color: 'orange',
+    },
+    {
+      name: 'GitHub',
+      link: 'https://github.com/YunYouJun',
+      icon: 'i-ri-github-line',
+      color: '#6e5494',
+    },
+    {
+      name: '哔哩哔哩',
+      link: 'https://space.bilibili.com/1579790',
+      icon: 'i-ri-bilibili-line',
+      color: '#FF8EB3',
+    },
+    {
+      name: 'E-Mail',
+      link: 'mailto:me@yunyoujun.cn',
+      icon: 'i-ri-mail-line',
+      color: '#8E71C1',
+    },
+    {
+      name: 'Travelling',
+      link: 'https://www.travellings.cn/go.html',
+      icon: 'i-ri-train-line',
+      color: 'var(--va-c-text)',
+    },
+  ],
+
+  search: {
+    enable: false,
+  },
+
+  sponsor: {
+    enable: false,
+    title: '我很可爱，请给我钱！',
+    methods: [
+      {
+        name: '支付宝',
+        url: 'https://cdn.yunyoujun.cn/img/donate/alipay-qrcode.jpg',
+        color: '#00A3EE',
+        icon: 'i-ri-alipay-line',
+      },
+      {
+        name: 'QQ 支付',
+        url: 'https://cdn.yunyoujun.cn/img/donate/qqpay-qrcode.png',
+        color: '#12B7F5',
+        icon: 'i-ri-qq-line',
+      },
+      {
+        name: '微信支付',
+        url: 'https://cdn.yunyoujun.cn/img/donate/wechatpay-qrcode.jpg',
+        color: '#2DC100',
+        icon: 'i-ri-wechat-pay-line',
+      },
+    ],
+  },
+})
